38ac13b973ae82fccec978a318b569edb4d6ffd0
Mykel Agathos <MThos@users.noreply.github.com>
2022-08-16 08:22:20 -0400

    changed layout of pricing to better center.

--- src/components/champion/Price.js
+++ src/components/champion/Price.js
@@ -7,12 +7,12 @@ const Price = (props) => {
     <div id="champion-price">
       <div id="champion-price-be">
         {static_data['price']['blueEssence']}
-      </div>
       <img id="champion-price-be-img" src='../images/compressed/BE_icon.webp' alt="Blue Essence" />
+      </div>
       <div id="champion-price-rp">
         {static_data['price']['rp']}
-      </div>
       <img id="champion-price-rp-img" src='../images/compressed/RP_icon.webp' alt="Riot Points" />
+      </div>
     </div>
   )
 }
