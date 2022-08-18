9180baa851e8f9b6f3949c5e11b00fcbcdfec010
Mykel Agathos <MThos@users.noreply.github.com>
2022-08-18 07:20:53 -0400

    added routes for privacy/terms/cookies/contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Footer from './components/core/Footer';
 import Champions from './components/core/Champions';
 import Champion from './components/champion/Champion';
 import Patch from './components/core/Patch';
+import Terms from './components/core/Terms';
+import Privacy from './components/core/Privacy';
+import Cookies from './components/core/Cookies';
+import Contact from './components/core/Contact';
 
 const App = () => {
   return (
@@ -14,8 +18,12 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Champions />} />
-          <Route path="/champions" element={<Champions />} />          
+          <Route path="/champions" element={<Champions />} />
           <Route path="/champion/*" element={<Champion />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/cookies" element={<Cookies />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
