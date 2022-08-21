5cc9a1448fc25d4c43a6ad845e8393e37f518949
Mykel Agathos <MThos@users.noreply.github.com>
2022-08-21 08:43:21 -0400

    updated header image

--- src/components/core/Header.js
+++ src/components/core/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <a href="/champions">Champions</a>
-      <img src={process.env.PUBLIC_URL + "/images/compressed/logo-L.png"} alt="League Census" />
-      <a href="/items">Items</a>
+      <a href="/champions" className="header-button">CHAMPIONS</a>
+      <img id="header-img" src={process.env.PUBLIC_URL + "/images/compressed/logo-outside-L-circle.png"} alt="League Census" />
+      <a href="/items" className="header-button">ITEMS</a>
     </header>
   )
 }
