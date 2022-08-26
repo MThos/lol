2e6c5d93d9231ca6c9d2ceb8c98135d5d4599ac6
Mykel Agathos <MThos@users.noreply.github.com>
2022-08-26 15:38:29 -0400

    fixed champion-spell being an id - should have been a className

--- src/components/champion/Spell.js
+++ src/components/champion/Spell.js
@@ -22,58 +22,56 @@ const Spell = (props) => {
   //const notes = (static_data['abilities'][hotkey][0]['notes'] ? static_data['abilities'][hotkey][0]['notes'] : "");
   
   return (
-    <div id="champion-spell">
-      <div className="champion-spell">
-        <div className="champion-spells-row-top">
-          <div className="text-align-left">
-            <img className="champion-spell-img" src={icon} alt={name} />
-          </div>
-          <div className="champion-spell-name text-align-center">
-            {name}
-          </div>
-          <div className="champion-spell-key text-align-right bold">
-            KEY &#183; {hotkey}
-          </div>
+    <div className="champion-spell">
+      <div className="champion-spells-row-top">
+        <div className="text-align-left">
+          <img className="champion-spell-img" src={icon} alt={name} />
         </div>
-        <div className="champion-spells-row">
-          <div className="text-align-left">
-            <span className="champion-spells-row-key">Targetting</span> <span className="bold">{targetting}</span>
-          </div>
-          <div className="text-align-center">
-            <span className="champion-spells-row-key">Affects</span> <span className="bold">{affects}</span>
-          </div>
-          <div className="text-align-right">
-            <span className="champion-spells-row-key">Element</span> <span className={`bold ${ColorOptions("element", element_lower)}`}>{element_lower}</span>
-          </div>
+        <div className="champion-spell-name text-align-center">
+          {name}
         </div>
-        <div className="champion-spells-row">
-          <div className="text-align-left">
-            <span className="champion-spells-row-key">Range</span> <span className="bold">{range}</span>
-          </div>
-          <div className="text-align-center">
-            <span className="champion-spells-row-key">Radius</span> <span className="bold">{radius}</span>
-          </div>
-          <div className="text-align-right">
-            <span className="champion-spells-row-key">Resource</span> <span className={`bold ${ColorOptions("resource", resource_lower)}`}>{resource_lower}</span>
-          </div>
+        <div className="champion-spell-key text-align-right bold">
+          KEY &#183; {hotkey}
         </div>
-        <div className="champion-spells-row">
-          <div className="text-align-left">
-            <span className="champion-spells-row-key">Cooldown</span> <span className="bold color-green">{cooldown_mods}</span>
-          </div>
-          <div className="text-align-center">
-            <span className="champion-spells-row-key">Cost</span> <span className="bold color-red">{cost_mods}</span>
-          </div>
-          <div className="text-align-right">
-            <span className="champion-spells-row-key">Shieldable</span> <span className={`bold ${ColorOptions("shield", shieldable)}`}>{shieldable}</span>
-          </div>
-        </div>
-        {
-          static_data['abilities'][hotkey][0]['effects'].map((key) => (
-            SpellModifiers(key)
-          ))
-        }
       </div>
+      <div className="champion-spells-row">
+        <div className="text-align-left">
+          <span className="champion-spells-row-key">Targetting</span> <span className="bold">{targetting}</span>
+        </div>
+        <div className="text-align-center">
+          <span className="champion-spells-row-key">Affects</span> <span className="bold">{affects}</span>
+        </div>
+        <div className="text-align-right">
+          <span className="champion-spells-row-key">Element</span> <span className={`bold ${ColorOptions("element", element_lower)}`}>{element_lower}</span>
+        </div>
+      </div>
+      <div className="champion-spells-row">
+        <div className="text-align-left">
+          <span className="champion-spells-row-key">Range</span> <span className="bold">{range}</span>
+        </div>
+        <div className="text-align-center">
+          <span className="champion-spells-row-key">Radius</span> <span className="bold">{radius}</span>
+        </div>
+        <div className="text-align-right">
+          <span className="champion-spells-row-key">Resource</span> <span className={`bold ${ColorOptions("resource", resource_lower)}`}>{resource_lower}</span>
+        </div>
+      </div>
+      <div className="champion-spells-row">
+        <div className="text-align-left">
+          <span className="champion-spells-row-key">Cooldown</span> <span className="bold color-green">{cooldown_mods}</span>
+        </div>
+        <div className="text-align-center">
+          <span className="champion-spells-row-key">Cost</span> <span className="bold color-red">{cost_mods}</span>
+        </div>
+        <div className="text-align-right">
+          <span className="champion-spells-row-key">Shieldable</span> <span className={`bold ${ColorOptions("shield", shieldable)}`}>{shieldable}</span>
+        </div>
+      </div>
+      {
+        static_data['abilities'][hotkey][0]['effects'].map((key) => (
+          SpellModifiers(key)
+        ))
+      }
     </div>
   )
 }
