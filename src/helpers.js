27b5786257778460da409d84d4e9f9eadf74dadb
MThos <MThos@users.noreply.github.com>
2022-09-01 06:30:19 -0400

    new regex for isExactMatch check

--- src/helpers.js
+++ src/helpers.js
@@ -32,29 +32,37 @@ export function SpellColors(unit, attribute) {
   unit = unit.toLowerCase();
   attribute = attribute.toLowerCase();
 
+  const escapeRegExpMatch = function(s) {
+    return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
+
+  const isExactMatch = (str, match) => {
+    return new RegExp(`\\b${escapeRegExpMatch(match)}\\b`).test(str)
+  }
+
   if (unit.includes('health')) {
     return "color-red";
-  } else if (unit.includes('ap')) {
+  } else if (isExactMatch(unit, 'ap')) {
     return "color-blue";
-  } else if (unit.includes('ad')) {
+  } else if (isExactMatch(unit, 'ad')) {
     return "color-brown";
-  } else if (attribute.includes('health')) {
+  } else if (isExactMatch(attribute, 'health')) {
     return "color-red";
-  } else if (attribute.includes('magic')) {
+  } else if (isExactMatch(attribute, 'magic')) {
     return "color-blue";
-  } else if (attribute.includes('ap')) {
+  } else if (isExactMatch(attribute, 'ap')) {
     return "color-blue";
-  } else if (attribute.includes('physical')) {
+  } else if (isExactMatch(attribute, 'physical')) {
     return "color-brown";
-  } else if (attribute.includes('ad')) {
+  } else if (isExactMatch(attribute, 'ad')) {
     return "color-brown";
-  } else if (attribute.includes('shield')) {
+  } else if (isExactMatch(attribute, 'shield')) {
     return "color-green";
-  } else if (attribute.includes('speed')) {
+  } else if (isExactMatch(attribute, 'speed')) {
     return "color-yellow";
-  } else if (attribute.includes('stun')) {
+  } else if (isExactMatch(attribute, 'stun')) {
     return "color-orange";
-  } else if (attribute.includes('slow')) {
+  } else if (isExactMatch(attribute, 'slow')) {
     return "color-orange";
   } else {
     return "";
@@ -63,13 +71,14 @@ export function SpellColors(unit, attribute) {
 
 export function ModifierUnits(unit) {
   if (unit.includes('AD')) {
-    return " (AD Modifier %)";
+    return "AD Modifier: ";
   } else if (unit.includes('AP')) {
-    return " (AP Modifier %)";
+    return "AP Modifier: ";
   } else if (unit === "") {
-    return " (Baseline)";
+    return "Baseline: ";
   } else {
-    return ` (${unit})`;
+    unit = unit.replace("%", "Percent");
+    return `${unit}: `;
   }
 }
 
