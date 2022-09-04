51ff013ae133f278cf96aa55738ceb01bfe1db3a
MThos <MThos@users.noreply.github.com>
2022-09-04 06:00:21 -0400

    added item components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/core/Header';
 import Footer from './components/core/Footer';
 import Champions from './components/core/Champions';
 import Champion from './components/champion/Champion';
+import Items from './components/core/Items';
+import Item from './components/item/Item';
 import Patch from './components/core/Patch';
 import Terms from './components/core/Terms';
 import Privacy from './components/core/Privacy';
@@ -21,6 +23,8 @@ const App = () => {
           <Route path="/" element={<Champions />} />
           <Route path="/champions" element={<Champions />} />
           <Route path="/champion/*" element={<Champion />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/item/*" element={<Item />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/cookies" element={<Cookies />} />
