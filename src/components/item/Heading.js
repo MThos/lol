d5227a822c91c2e242fed9632e303fee14ef2cf6
MThos <MThos@users.noreply.github.com>
2022-09-05 00:27:31 -0400

    updated classNames

--- src/components/item/Heading.js
+++ src/components/item/Heading.js
@@ -6,23 +6,23 @@ const Heading = (props) => {
 
   if (image && typeof(image) === 'object' && Object.keys(image).length === 2) {
     return (
-      <div className="champion-heading">
-        <img className="champion-heading-img" src={`../images/compressed/${image[0]}.webp`} alt={image} />
+      <div className="item-heading">
+        <img className="item-heading-img" src={`../images/compressed/${image[0]}.webp`} alt={image} />
         {title.toUpperCase()}
-        <img className="champion-heading-img" src={`../images/compressed/${image[1]}.webp`} alt={image} />
+        <img className="item-heading-img" src={`../images/compressed/${image[1]}.webp`} alt={image} />
       </div>
     )
   } else if (image && typeof(image) === 'string') {
     return (
-      <div className="champion-heading">
-        <img className="champion-heading-img" src={`../images/compressed/${image}.webp`} alt={image} />
+      <div className="item-heading">
+        <img className="item-heading-img" src={`../images/compressed/${image}.webp`} alt={image} />
         {title.toUpperCase()}
-        <img className="champion-heading-img" src={`../images/compressed/${image}.webp`} alt={image} />
+        <img className="item-heading-img" src={`../images/compressed/${image}.webp`} alt={image} />
       </div>
     )
   } else {
     return (
-      <div className="champion-heading">
+      <div className="item-heading">
         {title.toUpperCase()}
       </div>
     )
