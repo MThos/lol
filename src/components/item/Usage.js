3fbbec15f9e82a4ef3ce9ba46ab5e370fddb17b1
MThos <MThos@users.noreply.github.com>
2022-09-05 00:27:14 -0400

    Create Usage.js

---
+++ src/components/item/Usage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Heading from './Heading';
+
+const Usage = (props) => {
+  const static_data = props.static_data;
+
+  if (static_data['shop']['tags'].length > 0) {
+    return (
+      <div id="item-usages">
+        <Heading title="usage" />
+        <div id="item-usage-flex">
+          { Tags(static_data) }
+        </div>      
+      </div>
+    )
+  } else {
+    return null;
+  }  
+}
+
+const Tags = (static_data) => {
+  const tag = static_data['shop']['tags'];
+
+  return (
+    <div className="item-usage" key={tag}>
+      {tag.join(' / ').toLowerCase().replace(/[^a-zA-Z / ]/g, " ")}
+    </div>
+  )
+}
+
+export default Usage;
