0c82cbab5601a9e5d9275b8974bad4f840f57c7d
MThos <MThos@users.noreply.github.com>
2022-09-05 07:37:39 -0400

    Create Stats.js

---
+++ src/components/item/Stats.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Heading from './Heading';
+import { StatCrossReference } from '../../helpers';
+
+const Stats = (props) => {
+  const static_data = props.static_data;
+
+  if (Object.keys(static_data['stats']).length > 0) {
+    return (
+      <div id="item-stats">
+        <Heading title="STATISTICS" />
+        <div id="item-stat-flex">
+          <div className="item-stat-top">
+            <div>Stat</div>
+            <div>Modifier</div>
+            <div>Value</div>
+          </div>
+          { Stat(static_data) }
+        </div>
+      </div>
+    )
+  } else {
+    return null;
+  }  
+}
+
+const Stat = (static_data) => {
+  const stats_data = static_data['stats'];
+  const stat_collection = [];
+  const stat_arr = [];
+
+  Object.keys(stats_data).forEach(key => {
+    Object.entries(stats_data[key]).forEach(stat_type => {
+      if (stat_type[1] > 0) {
+        stat_collection.push([StatCrossReference(key), stat_type[0], stat_type[1]]);
+      }
+    })
+  });
+
+  stat_collection.map((key => (
+    stat_arr.push(
+      <div className="item-stat" key={key}>
+        <div>{key[0]}</div>
+        <div>{key[1]}</div>
+        <div>{key[2]}</div>
+      </div>
+    )
+  )));
+
+  return (
+    stat_arr
+  )
+}
+
+export default Stats;
