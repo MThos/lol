c62b7aa5ee4cee917e915f457651a470c5af0ff6
MThos <MThos@users.noreply.github.com>
2022-09-06 06:43:03 -0400

    added a sort on items (alphabetically)

--- src/components/core/Items.js
+++ src/components/core/Items.js
@@ -7,9 +7,7 @@ const Items = () => {
     return (
       <section>
         <div id="items" className="fade-in">
-          {Object.keys(data_dragon).length > 0 && Object.keys(data_dragon.data).map((key, i) => (
-            ItemCheck(key, i)
-          ))}
+          {ItemList(data_dragon)}
         </div>
       </section>
     )
@@ -18,18 +16,34 @@ const Items = () => {
   }  
 }
 
-const ItemCheck = (key, i) => {
-  const name = data_dragon['data'][key]['name'];
-  if (name.toString().substring(0, 1) !== "<" && name !== "Gangplank Placeholder") {
-    return(
-      <div className="item" key={i}>
-        <a href={`/item/${key}`}>
-          <img src={`images/compressed/item/${data_dragon['data'][key]['image']['full'].slice(0, -4)}.webp`} loading="lazy" width="120px" alt={name} />
+const ItemList = (data_dragon) => {
+  const item_arr = [];
+  const item_list = [];
+
+  Object.keys(data_dragon).length > 0 && Object.keys(data_dragon.data).map((key, i) => {
+    const name = data_dragon['data'][key]['name'];
+
+    if (name.toString().substring(0, 1) !== "<" && name !== "Gangplank Placeholder") {
+      item_arr.push([name, [key, i]]);
+    }
+
+    return null;
+  })
+
+  item_arr.sort();
+
+  item_arr.forEach(key => (
+    item_list.push(
+      <div className="item" key={key[1][1]}>
+        <a href={`/item/${key[1][0]}`}>
+          <img src={`images/compressed/item/${data_dragon['data'][key[1][0]]['image']['full'].slice(0, -4)}.webp`} loading="lazy" width="80px" alt={key[0]} />
         </a>
-        <p>{name}</p>
-      </div>
+        <p>{key[0]}</p>
+    </div>
     )
-  }  
+  ));
+
+  return item_list;
 }
 
 export default Items;
