442773b52a377ac758ca7925f0f9ba6b21b50798
MThos <MThos@users.noreply.github.com>
2022-09-06 04:45:43 -0400

    StatModCrossReference helper function utilized

--- src/components/item/Stats.js
+++ src/components/item/Stats.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Heading from './Heading';
-import { StatCrossReference } from '../../helpers';
+import { StatCrossReference, StatModCrossReference } from '../../helpers';
 
 const Stats = (props) => {
   const static_data = props.static_data;
@@ -8,13 +8,8 @@ const Stats = (props) => {
   if (Object.keys(static_data['stats']).length > 0) {
     return (
       <div id="item-stats">
-        <Heading title="STATISTICS MODIFIERS" />
+        <Heading title="STAT MODIFIERS" />
         <div id="item-stat-flex">
-          <div className="item-stat-top">
-            <div>Stat</div>
-            <div>Modifier</div>
-            <div>Value</div>
-          </div>
           { Stat(static_data) }
         </div>
       </div>
@@ -32,7 +27,7 @@ const Stat = (static_data) => {
   Object.keys(stats_data).forEach(key => {
     Object.entries(stats_data[key]).forEach(stat_type => {
       if (stat_type[1] > 0) {
-        stat_collection.push([StatCrossReference(key), stat_type[0], stat_type[1]]);
+        stat_collection.push([StatCrossReference(key), StatModCrossReference(stat_type[0], stat_type[1])]);
       }
     })
   });
@@ -40,9 +35,7 @@ const Stat = (static_data) => {
   stat_collection.map((key => (
     stat_arr.push(
       <div className="item-stat" key={key}>
-        <div>{key[0]}</div>
-        <div>{key[1]}</div>
-        <div>{key[2]}</div>
+        <div>{key[0]} = <span className="color-green">+{key[1]}</span></div>
       </div>
     )
   )));
